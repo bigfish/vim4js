ea0f001c245fb5a0185387495489d3e6e6dd0698
David Wilhelm <dewilhelm@gmail.com>
2011-12-01 22:12:38 -0500

    removed jstd template

--- templates/jstest.js
+++
@@ -1,8 +0,0 @@
-/*global TestCase PACKAGE fail assertTrue*/
-/*jslint newcap:false*/
-TestCase("CLASSNAMETest", {
-    testConstructor: function () {
-        var instance = new PACKAGE.CLASSNAME();
-        assertNotNull("Ensure constructor returns an object", instance);
-    }
-});
